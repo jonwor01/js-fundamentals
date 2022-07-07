62feeb0c4400ab6233a28a8322ea2ed0f54fb2a7
Jeffrey Onwordi <jonwor01@hotmail.com>
2022-07-07 07:11:38 +0100

    data types exercises

--- src/variables/assignment.js
+++ src/variables/assignment.js
@@ -3,6 +3,7 @@ let firstNumber = 10
 firstNumber = 0
 
 // TODO: Set the value of firstNumber below so the tests pass
+firstNumber = 0
 
 // TODO: Change the code below so that the tests pass
 const secondNumber = 0 // edit this value
